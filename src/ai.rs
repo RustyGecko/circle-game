3a093d7af08562705c403299580d2c8357bc504a
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2015-05-07 16:13:28 +0200

    Inverted button pattern to fix ai bug

--- src/ai.rs
+++ src/ai.rs
@@ -9,7 +9,7 @@ pub fn get_simulate_buttons(env: &GameEnv) -> u32 {
     let (gap_1_start, gap_1_end) = env.obstacle.gap1;
     let gap1 = ((gap_1_start + gap_1_end) / 2, gap_y);
 
-    match env.obstacle.gap2 {
+    let button_pattern = match env.obstacle.gap2 {
         Some((gap_2_start, gap_2_end)) => {
             let gap2 = ((gap_2_start + gap_2_end) / 2, gap_y);
 
@@ -29,25 +29,27 @@ pub fn get_simulate_buttons(env: &GameEnv) -> u32 {
             }
         },
         None => both_go_to_gap(env, gap1)
-    }
+    };
+
+    !button_pattern
 
 }
 
-fn distance(circle: &Circle, gap: (i32,i32)) -> i32 {
+fn distance(circle: &Circle, gap_center: (i32,i32)) -> i32 {
     let x = circle.rect.dx + 25;
     let y = circle.rect.dy + 25;
-    let (g_x, g_y) = gap;
+    let (g_x, g_y) = gap_center;
 
     let pow_2 = |a| a * a;
 
     pow_2(x - g_x) + pow_2(y - g_y)
 }
 
-fn both_go_to_gap(env: &GameEnv, gap: (i32, i32)) -> u32 {
+fn both_go_to_gap(env: &GameEnv, gap_center: (i32, i32)) -> u32 {
 
     let mut buttons = 0;
 
-    let gap_x = gap.0;
+    let gap_x = gap_center.0;
 
     // Move circle 1 on x axis
     if (env.circle1.rect.dx + 25) < gap_x {
@@ -63,8 +65,8 @@ fn both_go_to_gap(env: &GameEnv, gap: (i32, i32)) -> u32 {
         buttons |= 0x10;
     }
 
-    let diff1 = distance(&env.circle1, gap);
-    let diff2 = distance(&env.circle2, gap);
+    let diff1 = distance(&env.circle1, gap_center);
+    let diff2 = distance(&env.circle2, gap_center);
 
     if diff1 < diff2 {
         buttons |= 0x80;
@@ -81,9 +83,9 @@ fn both_go_to_gap(env: &GameEnv, gap: (i32, i32)) -> u32 {
     buttons
 }
 
-fn go_to_gap(env: &GameEnv, circle: u32, gap: (i32, i32)) -> u32 {
+fn go_to_gap(env: &GameEnv, circle: u32, gap_center: (i32, i32)) -> u32 {
 
-    let gap_x = gap.0;
+    let gap_x = gap_center.0;
 
     if circle == 0 {
         let x = env.circle1.rect.dx + 25;
